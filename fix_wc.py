075bae3692a596a26114092ba53a673315ff23ea
Darian Hickman <darian.hickman@mavgo.com>
2026-09-13 11:41:21 -0500

    clean up

--- fix_wc.py
+++
@@ -1,8 +0,0 @@
-import re
-
-for f in ["pkg/strategy/wc_4day_hold.go", "pkg/strategy/whitings_creek.go"]:
-    with open(f, "r") as file:
-        content = file.read()
-    content = content.replace('"data/wc_master_backtest.db", ', '')
-    with open(f, "w") as file:
-        file.write(content)
